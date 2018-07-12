ebbc9916f10594b429b689b36d69783c0db7faa7
Chely Ho <adept_inept@yahoo.com>
2018-07-12 20:58:53 +0000

    Done.

--- index.js
+++ index.js
@@ -10,13 +10,28 @@ function createIssue() {
 function showResults(json) {
 }
 
+// GET /repos/:owner/:repo
+// GET /repos/:owner/:repo/forks
+// POST /repos/:owner/:repo/forks
+
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}`, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => console.log(json))
+
+    console.log("//ignored") //gitignore
+    console.log("#ignored") #gitignore
+    // .then(json => showForkedRepo(json))
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  // return ''
+  return ENV['TOKEN']
 }
