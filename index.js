e888666ea43bbbcdcd38c8daa8bc18f7ae78c9b5
Chely Ho <adept_inept@yahoo.com>
2018-07-12 22:31:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function forkRepo() {
 }
 
 function showForkedRepo(repo) {
-  let repoHTML = `<a id="repo-link" href="${repo.html_url}">${repo.name}</a>`
+  let repoHTML = `<a id="repo-link" data-owner="${repo.author.login}" href="${repo.html_url}">${repo.name}</a>`
   $("#results").append(repoHTML)
   getIssues(repo)
 }
