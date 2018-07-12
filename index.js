fecb43784f051a2795c4d3e82c7d58f751523b86
Chely Ho <adept_inept@yahoo.com>
2018-07-12 21:53:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ function createIssue() {
 function showResults(json) {
 }
 
-// GET /repos/:owner/:repo
-// GET /repos/:owner/:repo/forks
-// POST /repos/:owner/:repo/forks
-
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   fetch(`https://api.github.com/repos/${repo}`, {
@@ -29,6 +25,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  // return ''
   return ''
 }
