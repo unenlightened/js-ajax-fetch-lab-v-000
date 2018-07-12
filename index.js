8e8d82475573e67ad1c0f947f89d85f32d0cc140
Chely Ho <adept_inept@yahoo.com>
2018-07-12 22:49:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-function getIssues() {
-}
+let userName;
+let repoName;
 
-function showIssues(json) {
+function getToken() {
+  //change to your token to run in browser, but set
+  //back to '' before committing so all tests pass
+  return ''
 }
-
-function createIssue() {
-}
-
-function showResults(json) {
-}
-
-// GET /repos/:owner/:repo
-// GET /repos/:owner/:repo/forks
-// POST /repos/:owner/:repo/forks
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  repoName = 'javascript-fetch-lab'
+  const repo = `learn-co-curriculum/${repoName}`
   fetch(`https://api.github.com/repos/${repo}`, {
+    method: 'post',
     headers: {
       Authorization: `token ${getToken()}`
     }
   })
     .then(res => res.json())
-    .then(json => console.log(json))
-
-    console.log("//sdf") //gitignore
-    console.log("#sdfa") #gitignore
-
-    // .then(json => showForkedRepo(json))
+    .then(json => showForkedRepo(json))
 }
 
-function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  // return ''
-  return ENV['TOKEN']
+function showForkedRepo(repo) {
+  userName = '' //pull from the repo author data
+  let repoHTML = `<a id="repo-link" href="${repo.html_url}">${repo.name}</a>`
+  $("#results").append(repoHTML)
+  getIssues(repo)
+}
+
+function createIssue() {
+  let title = document.getElementById('title').value
+  let body = document.getElementById('body').value
+
+  const repo = `${userName}/${repoName}`
+
+  let postData = { title: title, body: body}
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  })
+}
+
+function getIssues(repo) {
+  const repoLink = `${userName}/${repoName}`
+  fetch(`https://api.github.com/repos/${repoLink}/issues`, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showIssues(json))
+}
+
+function showIssues(issues) {
+
+  $("#results").append(repoHTML)
 }
